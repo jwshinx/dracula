010b8bb4c4b303ff4333c02fc8abf38424bf81d5
Joel Shin <jwshin@gmail.com>
2013-04-16 14:13:44 -0700

    untracked some rspec files

--- spec/lib/dracula/version_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "Version" do
- it "should be true" do
-  Dracula::VERSION.wont_be_nil
- end
-end
-
--- spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-#require 'subscription'
-#require 'person' 
-#require 'address'
-#require 'pet'
-require 'factory_girl'
-
-FactoryGirl.find_definitions
-
